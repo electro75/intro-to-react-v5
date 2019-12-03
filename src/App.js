f452c435d5fec6a11e94ece47274d2aea19593d4
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-12-03 21:35:38 +0530

    converts files to JSX

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt me"),
-    React.createElement(Pet, { name: "Theo", animal: "Dog", breed: "Pug" }),
-    React.createElement(Pet, { name: "Pepper", animal: "Dog", breed: "Corgi" }),
-    React.createElement(Pet, { name: "Felix", animal: "Tortoise", breed: "??" })
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h1", {}, "Adopt me"),
+  //   React.createElement(Pet, { name: "Theo", animal: "Dog", breed: "Pug" }),
+  //   React.createElement(Pet, { name: "Pepper", animal: "Dog", breed: "Corgi" }),
+  //   React.createElement(Pet, { name: "Felix", animal: "Tortoise", breed: "??" })
+  // );
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Theo" animal="Dog" breed="Pug" />
+      <Pet name="Pepper" animal="Dog" breed="Corgi" />
+      <Pet name="Felix" animal="Tortoise" breed="??" />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
