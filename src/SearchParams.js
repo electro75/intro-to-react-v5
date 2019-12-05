74d87a775e8b2d02f75c3a3160d2b16d91dccba5
Sahil Naikdhure <sanaikdhure@gmail.com>
2019-12-05 09:26:56 +0530

    adds a custom hook

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  // this is a hook
+  // a hook never goes into if/for loop. (basically any unpredictable logic)
+  const [location, setLocation] = useState("Seattle, WA");
+  // const [animal, setAnimal] = useState("Dog");
+  // const [breed, setBreed] = useState("breed");
+  const [breeds, setBreeds] = useState([]);
+
+  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">Location</label>
-        <input id="location" value={location} />
+        <input
+          id="location"
+          value={location}
+          placeholder="Location"
+          onChange={e => setLocation(e.target.value)}
+        />
         <button>Submit</button>
+        {/* <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={e => setAnimal(e.target.value)} // causes accessibility issue because tab does not work in some browsers.
+          onBlur={e => setAnimal(e.target.value)} // fixes accessibility issue.
+        >
+          <option>All</option>
+          {ANIMALS.map(animal => {
+            return (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            );
+          })}
+        </select> */}
+        <AnimalDropdown />
+        {/* <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          value={breed}
+          onChange={e => setBreed(e.target.value)}
+          onBlur={e => setBreed(e.target.value)}
+          disabled={breeds.length === 0}
+        >
+          <option value="All">All</option>
+          {breeds.map(breedString => (
+            <option value={breedString} key={breedString}>
+              {breedString}
+            </option>
+          ))}
+        </select> */}
+        <BreedDropdown />
       </form>
     </div>
   );
